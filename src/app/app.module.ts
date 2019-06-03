4d31bcf845eb776f42faee0340f0fc82816efcb3
Antony <amwaniki180@gmail.com>
2019-06-03 22:42:47 +0300

    added component form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteComponent
+    QuoteComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule
