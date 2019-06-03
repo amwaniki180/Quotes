9f0873bdc0dace3f77f4d06c83d9f769b153c91b
Antony <amwaniki180@gmail.com>
2019-06-03 23:18:34 +0300

    add votes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quote = [
+  
+  ];
+  completeQuote(isComplete,index){
+    if(isComplete){
+      let toDelete = confirm(`are you ${this.quote[index].name}`)
+      if (toDelete){
+        this.quote.splice(index,1);
+      }
+    }
+  }
+  formReceive(quote) {
+    this.quote.push(quote);
+
+  }
+  addVote(index) {
+    this.quote[index].upvote =+1;
+  }
+
+  minusVote(index) {
+    this.quote[index].downvote =+1;
+    
+  };
 
   constructor() { }
 
