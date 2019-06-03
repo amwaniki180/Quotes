fe0c819ffa6fb4acdf9db468b1397c35d73505f3
Antony <amwaniki180@gmail.com>
2019-06-03 23:07:08 +0300

    submit quote

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
   newQuote = new Quote(0, '', ' ', ' ', new Date(), 0 , 0 );
+  
+  @Output() addQuote = new EventEmitter<Quote>();
 
+  submitQuote() {
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote(0, '', ' ', ' ', new Date(), 0 , 0);
+  }
 
   constructor() { }
 
