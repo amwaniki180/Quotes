bb8aa4723f7d9729bf2ca30c6537ab0f342bf7dc
root <amwaniki180@gmail.com>
2019-06-04 15:28:00 +0300

    Auto-generated commit

--- src/app/quote.ts
+++
@@ -1,7 +0,0 @@
-export class Quote {
-    showDescription: boolean;
-  constructor(public id: number, public name: string, public author: string, public quotes: string, public completeDate: Date, public upvote: number, public downvote: number) {
-    this.showDescription = false;
-
-}
-}
